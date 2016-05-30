341a86a058c50a5dc8520f1ef64052bd4ffd6bab
Adel Lesuchevskiy <adelnator@gmail.com>
2016-05-30 19:49:43 +0300

    Cat remake

--- cat/cat.c
+++ cat/cat.c
@@ -1,7 +1,28 @@
 #include <unistd.h>
-#include "helpers.h"
 
 #define BUF_SIZE 4000
+
+ssize_t read_(int fd, void *buf, size_t count) {
+    ssize_t cur = 0;
+    while (cur < count) {
+        ssize_t res = read(fd, buf + cur, count - cur);
+        if (res == -1) return -1;
+        if (res == 0) break;
+        cur += res;
+    }
+    return cur;
+}
+
+ssize_t write_(int fd, const void *buf, size_t count) {
+    ssize_t cur = 0;
+    while (cur < count) {
+        ssize_t written = write(fd, buf + cur, count - cur);
+        if (written == -1) return -1;
+        if (written == 0) break;
+        cur += written;
+    }
+    return cur;
+}
 
 int main() {
     char buf[BUF_SIZE];
